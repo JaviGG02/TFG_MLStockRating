7d8a4336d4e8b180dcadfbbb77a9b3418c4a893a
JaviGG02 <98906550+JaviGG02@users.noreply.github.com>
2024-04-22 00:06:32 +0200

    trying to solve module not found errors

--- scripts/format_code_black.sh
+++ scripts/format_code_black.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-TARGET_DIRECTORY="../proto_app/backend/"
-
+TARGET_DIRECTORY="./proto_app/backend/"
+ls
 black $TARGET_DIRECTORY
+pwd
+cd "../"
+ls
+
 
 echo "No black issues found in the Python code."
