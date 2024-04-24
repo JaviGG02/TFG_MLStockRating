b8288feb5ca28cedbd13058888983b20613262d5
JaviGG02 <98906550+JaviGG02@users.noreply.github.com>
2024-04-24 18:30:15 +0200

    Reformateado frontend Añadido al pipeline las comprobaciones del fronted

--- proto_app/src/App.js
+++ proto_app/src/App.js
@@ -1,74 +1,82 @@
 // Importando librerías y paquetes
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 // Importando componentes
-import BuscadorTicker from './components/BuscadorTicker/BuscadorTicker';
-import OverviewComponent from './components/OverviewComponent/OverviewComponent';
-import PricePredictionChart from './components/PricePredictionChart/PricePredictionChart';
-import FinancialTable from './components/FinancialTable/FinancialTable';
+import BuscadorTicker from "./components/BuscadorTicker/BuscadorTicker";
+import OverviewComponent from "./components/OverviewComponent/OverviewComponent";
+import PricePredictionChart from "./components/PricePredictionChart/PricePredictionChart";
+import FinancialTable from "./components/FinancialTable/FinancialTable";
 
 // Importando estilos
-import './components/BuscadorTicker/BuscadorTicker.css'
-import './components/FinancialTable/FinancialTable.css'
-import './components/OverviewComponent/OverviewComponent.css';
-import './App.css';
+import "./components/BuscadorTicker/BuscadorTicker.css";
+import "./components/FinancialTable/FinancialTable.css";
+import "./components/OverviewComponent/OverviewComponent.css";
+import "./App.css";
 
 function App() {
-  
   const [data, setData] = useState({
     cargando: true,
     datosFinancieros: null,
     predicciones: null,
     calificacion: null,
-    mensajeError: ''
+    mensajeError: "",
   });
 
   useEffect(() => {
-    console.log(data)
+    console.log(data);
   }, [data]);
 
   const buscarTicker = (ticker) => {
-  
-    fetch('http://localhost:5000/api/datos', {
-      method: 'POST',
+    fetch("http://localhost:5000/api/datos", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({ ticker: ticker }),
     })
-    .then(response => response.json())
-    .then(responseData => {
-      if (!responseData.Error) { 
-        setData({ // No error
-          cargando: false,
-          datosFinancieros: responseData.datos_financieros, 
-          predicciones: responseData.prediccion, 
-          calificacion: responseData.calificacion,
-        }); 
-      }
-      else { // Error en backend
+      .then((response) => response.json())
+      .then((responseData) => {
+        if (!responseData.Error) {
+          setData({
+            // No error
+            cargando: false,
+            datosFinancieros: responseData.datos_financieros,
+            predicciones: responseData.prediccion,
+            calificacion: responseData.calificacion,
+          });
+        } else {
+          // Error en backend
+          setData({
+            cargando: false,
+            mensajeError: responseData.Error,
+          });
+        }
+      })
+      .catch((error) => {
+        // Error en frontend o backend
+        console.error("Error:", error);
         setData({
-          cargando: false, 
-          mensajeError: responseData.Error
+          ...data,
+          cargando: false,
+          mensajeError: "An error occured while loading the data",
         });
-      }
-      
-    })
-    .catch(error => { // Error en frontend o backend
-      console.error('Error:', error);
-      setData({ ...data, cargando: false, mensajeError: 'An error occured while loading the data'}); 
-    });
+      });
   };
 
   return (
-    <div className='root-div'>
+    <div className="root-div">
       {data.cargando ? (
         <BuscadorTicker onSearch={buscarTicker} />
       ) : (
         <>
-          {data.datosFinancieros && data.predicciones && data.calificacion !== null ? (
+          {data.datosFinancieros &&
+          data.predicciones &&
+          data.calificacion !== null ? (
             <>
-              <OverviewComponent overview={data.datosFinancieros.OVERVIEW} calificacion={data.calificacion} />
+              <OverviewComponent
+                overview={data.datosFinancieros.OVERVIEW}
+                calificacion={data.calificacion}
+              />
               <div className="page-container">
                 <div className="graph-container">
                   <PricePredictionChart
@@ -77,16 +85,20 @@ function App() {
                   />
                 </div>
                 <div className="table-container">
-                  <FinancialTable 
-                    incomeStatement={data.datosFinancieros.INCOME_STATEMENT.annualReports} 
-                    balanceSheet={data.datosFinancieros.BALANCE_SHEET.annualReports} 
+                  <FinancialTable
+                    incomeStatement={
+                      data.datosFinancieros.INCOME_STATEMENT.annualReports
+                    }
+                    balanceSheet={
+                      data.datosFinancieros.BALANCE_SHEET.annualReports
+                    }
                     cashFlows={data.datosFinancieros.CASH_FLOW.annualReports}
                   />
                 </div>
               </div>
             </>
           ) : (
-            <p className='error-message'>Error: {data.mensajeError}</p>
+            <p className="error-message">Error: {data.mensajeError}</p>
           )}
         </>
       )}
--- proto_app/src/components/PricePredictionChart/PricePredictionChart.js
+++ proto_app/src/components/PricePredictionChart/PricePredictionChart.js
@@ -1,57 +1,83 @@
-import React from 'react';
-import { Line } from 'react-chartjs-2';
-import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, TimeScale } from 'chart.js';
-import 'chartjs-adapter-date-fns';
-ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, TimeScale);
-
-
+import React from "react";
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  TimeScale,
+} from "chart.js";
+import "chartjs-adapter-date-fns";
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  TimeScale,
+);
 
 // Funcion para el renderizado del grafico de precios y predicciones
 const PricePredictionChart = ({ datosFinancieros, predicciones }) => {
-
   if (!datosFinancieros || !predicciones) {
     return <p>Cargando datos...</p>;
   }
 
-  const fechasReales = datosFinancieros ? Object.keys(datosFinancieros.TIME_SERIES_MONTHLY_ADJUSTED) : [];
+  const fechasReales = datosFinancieros
+    ? Object.keys(datosFinancieros.TIME_SERIES_MONTHLY_ADJUSTED)
+    : [];
   const fechasPredicciones = predicciones ? Object.keys(predicciones) : [];
-  const fechasUnificadas = [...new Set([...fechasReales, ...fechasPredicciones])].sort();
-  const preciosRealesRellenados = fechasUnificadas.map(fecha => fechasReales.includes(fecha) ? datosFinancieros.TIME_SERIES_MONTHLY_ADJUSTED[fecha] : null);
-  const preciosPrediccionesRellenados = fechasUnificadas.map(fecha => fechasPredicciones.includes(fecha) ? predicciones[fecha] : null);
+  const fechasUnificadas = [
+    ...new Set([...fechasReales, ...fechasPredicciones]),
+  ].sort();
+  const preciosRealesRellenados = fechasUnificadas.map((fecha) =>
+    fechasReales.includes(fecha)
+      ? datosFinancieros.TIME_SERIES_MONTHLY_ADJUSTED[fecha]
+      : null,
+  );
+  const preciosPrediccionesRellenados = fechasUnificadas.map((fecha) =>
+    fechasPredicciones.includes(fecha) ? predicciones[fecha] : null,
+  );
 
   const data = {
-  labels: fechasUnificadas,
-  datasets: [
+    labels: fechasUnificadas,
+    datasets: [
       {
-      label: 'Precio Real',
-      data: preciosRealesRellenados,
-      borderColor: 'rgb(75, 192, 192)',
-      backgroundColor: 'rgba(75, 192, 192, 0.5)',
+        label: "Precio Real",
+        data: preciosRealesRellenados,
+        borderColor: "rgb(75, 192, 192)",
+        backgroundColor: "rgba(75, 192, 192, 0.5)",
       },
       {
-      label: 'Predicción',
-      data: preciosPrediccionesRellenados,
-      borderColor: 'rgb(255, 99, 132)',
-      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        label: "Predicción",
+        data: preciosPrediccionesRellenados,
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
       },
-  ],
+    ],
   };
 
   const options = {
     scales: {
       x: {
-        type: 'time',
+        type: "time",
         time: {
-          unit: 'month',
+          unit: "month",
           min: fechasUnificadas[0],
           max: fechasUnificadas[fechasUnificadas.length - 1],
-          tooltipFormat: 'MMM yyyy',
-        }
+          tooltipFormat: "MMM yyyy",
+        },
       },
       y: {
         title: {
           display: true,
-          text: 'Precio:' + datosFinancieros.OVERVIEW.Currency,
+          text: "Precio:" + datosFinancieros.OVERVIEW.Currency,
         },
       },
     },
@@ -67,6 +93,6 @@ const PricePredictionChart = ({ datosFinancieros, predicciones }) => {
   };
 
   return <Line data={data} options={options} />;
-}
+};
 
 export default PricePredictionChart;
